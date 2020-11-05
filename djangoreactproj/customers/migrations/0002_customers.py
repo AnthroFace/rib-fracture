dcadae4c1e0b12a691d8fe684edf7a672c3b3282
Kealia <kealiaperrine@gmail.com>
2020-11-05 15:16:02 -0800

    set up front and backend, customer example

---
+++ djangoreactproj/customers/migrations/0002_customers.py
@@ -0,0 +1,13 @@
+from django.db import migrations
+
+def create_data(apps, schema_editor):
+    Customer = apps.get_model('customers', 'Customer')
+    Customer(first_name="Customer 001", last_name="Customer 001", email="customer001@email.com", phone="00000000", address="Customer 000 Address", description= "Customer 001 description").save()
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('customers', '0001_initial'),
+    ]
+    operations = [
+        migrations.RunPython(create_data),
+    ]
