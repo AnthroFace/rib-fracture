ac58d9bdc6da3f01efdd4368ede52a8dbcbac3f4
skylarglock <62079581+skylarglock@users.noreply.github.com>
2020-11-06 16:43:05 -0800

    -m "commit with basic two button homepage"

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          ANTHROFACE
+        </p>
+        <a
+          className="App-link"
+          href="insert link to next page"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          New Patient
+        </a>
+        <a
+          className="App-link"
+          href="insert link to next page"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Database
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
