65c9c11352d3d224201650e653509f4ba0006dbf
Mari Trombley <42785737+tignunu@users.noreply.github.com>
2020-12-01 00:06:19 -0800

    made and added logo to home page

--- djangoreactproj/frontend/src/components/Home.js
+++ djangoreactproj/frontend/src/components/Home.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import { Typography } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
+import './../App.css';
 
 class  Home  extends  Component {
     render() {
         return (
             <div>
-                <Typography variant="h1" component="h2" gutterBottom>
-                    Anthroface
-                </Typography>
+                <img src = "/images/AnthrofaceLogo1.png" alt=""/>
                 <Button variant="contained" href="/Patient">New Patient</Button>
                 <Button variant="contained" href="/Database">Database</Button>
                 <Button variant="contained" href="/Import">Import Data</Button>
@@ -18,3 +17,7 @@ class  Home  extends  Component {
       }
     }
     export  default  Home;
+
+    /*  <Typography variant="h1" component="h2" gutterBottom>
+         Anthroface
+        </Typography>*/
