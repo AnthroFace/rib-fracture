f9f018e75cea5ba3bd485bd40c3534e94253cf62
Gabrielle Talavera <gabrielle9talavera@gmail.com>
2020-12-01 23:08:06 -0800

    Added fracture model and serializer

--- djangoreactproj/patients/serializers.py
+++ djangoreactproj/patients/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from .models import Patient
+from .models import Patient, Fracture
 
+class FractureSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Fracture
+        fields = ('pk', 'location', 'completeness', 'fracture_type')
+
+# Connect both serializers maybe have many=True
 class PatientSerializer(serializers.ModelSerializer):
+    rib_fracture = FractureSerializer(many=True)
 
     class Meta:
         model = Patient 
