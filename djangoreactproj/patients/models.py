91fabd38c66cc8c12e053de8478ed0bf097c5ba5
Gabrielle Talavera <gabrielle9talavera@gmail.com>
2021-01-03 14:07:39 -0800

    Changed database to two models to make it easier for exports and for user input

--- djangoreactproj/patients/models.py
+++ djangoreactproj/patients/models.py
@@ -28,17 +28,10 @@ class Patient(models.Model):
 
 class Fracture(models.Model):
     location = models.CharField("Location", max_length=20)
+    completeness = models.IntegerField("Completeness")
+    fracture_type = models.CharField("Fracture Type", max_length=20)
+    cpr = models.CharField('CPR', max_length=1)
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name="rib_fracture")
 
     def __str__(self):
         return self.patient.case_id
-
-class Section(models.Model):
-    section = models.CharField("Location", max_length=20)
-    completeness = models.IntegerField("Completeness")
-    fracture_type = models.CharField("Fracture Type", max_length=20)
-    cpr = models.CharField('CPR', max_length=1)
-    fracture = models.ForeignKey(Fracture, on_delete=models.CASCADE, related_name="rib_section")
-
-    def __str__(self):
-        return self.fracture.patient.case_id
