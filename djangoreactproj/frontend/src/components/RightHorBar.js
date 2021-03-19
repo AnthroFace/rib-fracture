47cad62c19122eafe5b0692b6b85d65db4148682
Gabrielle Talavera <gabrielle.talavera@wahoofitness.com>
2021-03-19 15:20:36 -0700

    Connect right rib counts with right side

--- djangoreactproj/frontend/src/components/RightHorBar.js
+++ djangoreactproj/frontend/src/components/RightHorBar.js
@@ -38,14 +38,14 @@ function BarGroup(props) {
     }
 
     componentDidMount () {
-      var d = props.rib_count;
-      // console.log(this.props.location.state);
+      var d = this.props.rib_count;
+      console.log("YOYOYO", d);
       var bottomRightBarTotals ={
         bottomright1: d['rarib1']+d['rarib2']+d['rarib3']+d['rarib4']+d['rarib5']+d['rarib6']+d['rarib7']+d['rarib8']+d['rarib9']+d['rarib10']+d['rarib11']+d['rarib12'],
         bottomright2: d['ralrib1']+d['ralrib2']+d['ralrib3']+d['ralrib4']+d['ralrib5']+d['ralrib6']+d['ralrib7']+d['ralrib8']+d['ralrib9']+d['ralrib10']+d['ralrib11']+d['ralrib12'],
         bottomright3: d['rplrib1']+d['rplrib2']+d['rplrib3']+d['rplrib4']+d['rplrib5']+d['rplrib6']+d['rplrib7']+d['rplrib8']+d['rplrib9']+d['rplrib10']+d['rplrib11']+d['rplrib12'],
         bottomright4: d['rprib2']+d['rprib3']+d['rprib4']+d['rprib5']+d['rprib6']+d['rprib7']+d['rprib8']+d['rprib9']+d['rprib10'],
-      }
+      };
       
       this.setState({ data: [
         { value: bottomRightBarTotals.bottomright1},
@@ -53,11 +53,12 @@ function BarGroup(props) {
         {  value: bottomRightBarTotals.bottomright3 },
         {  value: bottomRightBarTotals.bottomright4},
       ]
-      })
+      });
     }  
     
     render() {
-      let barHeight =46
+      
+      let barHeight =46;
           
       let barGroups = this.state.data.map((d, i) => <g transform={`translate(0, ${i * barHeight})`}>
                                                       <BarGroup d={d} barHeight={barHeight} />
--- djangoreactproj/frontend/src/components/RightVertBar.js
+++ djangoreactproj/frontend/src/components/RightVertBar.js
@@ -20,6 +20,39 @@ function BarGroup(props) {
   class BarChart extends React.Component {
     state = {
       data: [
+        // { value: rightBarTotals.rightbar12},
+        // {  value: rightBarTotals.rightbar11},
+        // {  value: rightBarTotals.rightbar10},
+        // {  value: rightBarTotals.rightbar9},
+        // {  value: rightBarTotals.rightbar8},
+        // { value: rightBarTotals.rightbar7},
+        // { value: rightBarTotals.rightbar6},
+        // {  value: rightBarTotals.rightbar5},
+        // {  value: rightBarTotals.rightbar4},
+        // {  value: rightBarTotals.rightbar3},
+        // {  value: rightBarTotals.rightbar2},
+        // { value: rightBarTotals.rightbar1}
+      ]
+    }
+    componentDidMount (props) {
+      var d = this.props.rib_count;
+      // console.log(this.props.location.state);
+      var rightBarTotals = {
+        rightbar1: d['rplrib1']+d['ralrib1']+ d['rarib1'], 
+        rightbar2: d['rprib2']+ d['rplrib2']+ d['ralrib2+rarib2'],
+        rightbar3: d['rprib3'] + d['rplrib3']+ d['ralrib3+rarib3'],
+        rightbar4: d['rprib4']+ d['rplrib4'] + d['ralrib4+rarib4'],
+        rightbar5: d['rprib5'] + d['rplrib5'] + d['ralrib5'] +d['rarib5'],
+        rightbar6: d['rprib6']+ d['rplrib6'] + d['ralrib6'] + d['rarib6'],
+        rightbar7: d['rprib7'] + d['rplrib7'] + d['ralrib7'] + d['rarib7'],
+        rightbar8: d['rprib8'] + d['rplrib8'] + d['ralrib8']+ d['rarib8'],
+        rightbar9: d['rprib9'] + d['rplrib9'] + d['ralrib9'] + d['rarib9'],
+        rightbar10: d['rprib10'] + d['rplrib10'] + d['ralrib10'] + d['rarib10'],
+        rightbar11: d['rplrib11'] + d['ralrib11'] + d['rarib11'],
+        rightbar12: d['rplrib12'] + d['ralrib12'] +d['rarib12'],
+      }; 
+      
+      this.setState({ data: [
         { value: rightBarTotals.rightbar12},
         {  value: rightBarTotals.rightbar11},
         {  value: rightBarTotals.rightbar10},
@@ -33,9 +66,10 @@ function BarGroup(props) {
         {  value: rightBarTotals.rightbar2},
         { value: rightBarTotals.rightbar1}
       ]
-    }
-  
+      });
+    }  
     render() {
+      
       let barHeight =46
           
       let barGroups = this.state.data.map((d, i) => <g transform={`translate(0, ${i * barHeight})`}>
