ead3cd13988f69367fb4602d8a9fb21e2756dce2
Henry Sturm <henrysturm2@gmail.com>
2021-04-26 19:00:06 -0700

    Fixed axios put in searchbar

--- djangoreactproj/frontend/src/components/SearchBar.js
+++ djangoreactproj/frontend/src/components/SearchBar.js
@@ -392,7 +392,6 @@ class SearchBar extends Component {
     // e.preventDefault();
     axios
       .put(URL + "1", {
-        headers: {Authorization: `JWT ${localStorage.getItem('token')}`} ,
         age_start: this.state.age_start,
         age_end: this.state.age_end,
         weight_start: this.state.weight_start,
@@ -503,8 +502,9 @@ class SearchBar extends Component {
         rprib12: this.state.rprib12,
         rplrib12: this.state.rplrib12,
         ralrib12: this.state.ralrib12,
-      })
-      .then(function (response) {
+      }, 
+      {headers: {Authorization: `JWT ${localStorage.getItem('token')}`}}
+      ).then(function (response) {
         // console.log("EDITING", response);
       });
   };
